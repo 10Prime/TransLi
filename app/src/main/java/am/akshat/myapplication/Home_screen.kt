0383b6b00bfe2a935d0231c8060faa3396c5338a
Akshat Mudgal <105490890+10Prime@users.noreply.github.com>
2022-07-07 17:11:08 +0530

    Add files via upload

---
+++ app/src/main/java/am/akshat/myapplication/Home_screen.kt
@@ -0,0 +1,27 @@
+package am.akshat.myapplication
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+/
+class Home_screen : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_home_screen, container, false)
+        val translate = view.findViewById<FloatingActionButton>(R.id.translate)
+        translate.setOnClickListener { findNavController().navigate(R.id.action_home_screen_to_transLite) }
+        return view
+    }
+
+
+}
